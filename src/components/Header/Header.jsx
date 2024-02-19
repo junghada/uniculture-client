3d3df449b19f8928c26343af210f90e5852fc2c5
kje <jeaction63@gmail.com>
2024-02-20 08:21:54 +0900

    로그아웃 수정

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import { Link, useLocation } from "react-router-dom";
 import logoImg from "../../assets/logo.png";
 import axios from "axios";
+import "../PageLayout/PageLayout.css"
 
 const Header = () => {
     const [isNavOpen, setNavOpen] = useState(false);
@@ -56,53 +57,57 @@ const Header = () => {
     );
 
     return (
-        <nav className={`navbar navbar-expand-lg`} style={{ backgroundColor: '#C8DCA0' }}>
-            <div className="container-fluid">
-                <div className="d-flex align-items-center">
-                    <Link to="/" className={`navbar-brand ${activePage('/')}`} aria-current="page" style={{ fontFamily: "SuezOne"}}>
-                        <img src={logoImg} alt="Logo" width="30" height="24" className="d-inline-block align-text-top"/>
-                        UniCulture
-                    </Link>
-                </div>
+        // <div className="page-layout">
+            <nav className={`navbar navbar-expand-lg`} style={{ backgroundColor: '#C8DCA0' }}>
+                <div className="container-fluid">
+                    <div className="d-flex align-items-center" style={{paddingLeft: '100px'}}>
+                        <Link to="/" className={`navbar-brand ${activePage('/')}`} aria-current="page" style={{ fontFamily: "SuezOne"}}>
+                            <img src={logoImg} alt="Logo" width="30" height="24" className="d-inline-block align-text-top"/>
+                            UniCulture
+                        </Link>
+                    </div>
 
-                <div className={`ms-auto order-lg-last`}>
                     {isLogin ? (
-                        <button className={`btn nav-link`} style={{ backgroundColor: "#B3C693", padding: "5px 15px", marginRight: "10px" }}>
+                        <button className={`btn nav-link`} style={{ backgroundColor: "#B3C693", padding: "5px 15px", marginRight: "10px"}}>
                             로그아웃
                         </button>
                     ) : (
-                        <Link to="/sign-in" className={`btn nav-link`} style={{ backgroundColor: "#B3C693", padding: "5px 15px", marginRight: "10px" }}>
-                            로그인
-                        </Link>
+                        <>
+                            <div className={`ms-auto order-lg-last`}>
+                                <Link to="/sign-in" className={`btn nav-link`} style={{ backgroundColor: "#B3C693", padding: "5px 15px", marginRight: "10px" }}>
+                                    로그인
+                                </Link>
+                            </div>
+                            <div className={`order-lg-last`}>
+                                <Link to="/sign-up" className={`btn nav-link`} style={{ backgroundColor: "#B3C693", padding: "5px 15px", marginRight: "10px" }}>
+                                    회원가입
+                                </Link>
+                            </div>
+                        </>
                     )}
-                </div>
 
-                <div className={`order-lg-last`}>
-                    <Link to="/sign-up" className={`btn nav-link`} style={{ backgroundColor: "#B3C693", padding: "5px 15px", marginRight: "10px" }}>
-                        회원가입
-                    </Link>
-                </div>
+                    <button
+                        className="navbar-toggler"
+                        type="button"
+                        onClick={handleToggle}
+                        aria-expanded={isNavOpen ? 'true' : 'false'}
+                        style={{marginRight: '100px'}}
+                    >
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
 
-                <button
-                    className="navbar-toggler"
-                    type="button"
-                    onClick={handleToggle}
-                    aria-expanded={isNavOpen ? 'true' : 'false'}
-                >
-                    <span className="navbar-toggler-icon"></span>
-                </button>
-
-                <div className={`collapse navbar-collapse ${isNavOpen ? 'show' : ''}`}>
-                    <ul className="navbar-nav">
-                        <NavItem to="/" text="홈" activePage={activePage} />
-                        <NavItem to="/friends" text="친구" activePage={activePage} />
-                        <NavItem to="/study" text="스터디" activePage={activePage} />
-                        <NavItem to="/chatting" text="채팅" activePage={activePage} />
-                        <NavItem to="/profile" text="프로필" activePage={activePage} />
-                    </ul>
+                    <div className={`collapse navbar-collapse ${isNavOpen ? 'show' : ''}`}>
+                        <ul className="navbar-nav">
+                            <NavItem to="/" text="홈" activePage={activePage} />
+                            <NavItem to="/friends" text="친구" activePage={activePage} />
+                            <NavItem to="/study" text="스터디" activePage={activePage} />
+                            <NavItem to="/chatting" text="채팅" activePage={activePage} />
+                            <NavItem to="/profile" text="프로필" activePage={activePage} />
+                        </ul>
+                    </div>
                 </div>
-            </div>
-        </nav>
+            </nav>
+        // </div>
     );
 };
 
